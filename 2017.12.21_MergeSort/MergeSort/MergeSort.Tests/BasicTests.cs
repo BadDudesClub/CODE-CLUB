e5e2b5aaab7339f0c430ca09292306af1e0f81de
Brian Walsh <databasebrian@gmail.com>
2017-12-21 19:30:59 -0800

    did merge sort in class

---
+++ 2017.12.21_MergeSort/MergeSort/MergeSort.Tests/BasicTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MergeSort.Tests
+{
+    public class BasicTests
+    {
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3, 4 }, new int[] { 1, 2, 3, 4 })]
+        [InlineData(new int[]{ 2, 4, 3, 1 }, new int[]{1, 2, 3, 4})]
+        public void FirstTest(int[] input, int[] expectedOutput)
+        {
+            var output = MergeSort.SortItSucka(input);
+            
+            Assert.True(output.SequenceEqual(expectedOutput));
+        }
+    }
+}
---
+++ 2017.12.21_MergeSort/MergeSort/MergeSort/MergeSort.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSort
+{
+    public class MergeSort
+    {
+        public static int[] SortItSucka(int[] input)
+        {
+            if (input.Length == 1)
+            {
+                return input;
+            }
+
+            //split it
+            var side1 = SortItSucka(input.Take(input.Length / 2).ToArray());
+            var side2 = SortItSucka(input.Skip(side1.Count()).ToArray());
+
+            // merge
+            int i = 0, j = 0;
+            var result = new int[input.Length];
+            while (i < side1.Length || j < side2.Length)
+            {
+                if (i >= side1.Length)
+                {
+                    result[i + j] = side2[j++];
+                }
+                else if (j >= side2.Length)
+                {
+                    result[i + j] = side1[i++];
+                }
+                else if (side1[i] < side2[j])
+                {
+                    result[i + j] = side1[i++];
+                }
+                else
+                {
+                    result[i + j] = side2[j++];
+                }
+            }
+
+            return result;
+        }
+
+    }
+}
